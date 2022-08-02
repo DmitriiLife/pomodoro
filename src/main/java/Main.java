5d12e933ab706ceeb6b14e0833f3f3d609d0b077
mac <dmitri-shadri@yandex.ru>
2022-08-02 17:32:10 +0300

    add README.md &  modified Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-
 public class Main {
 
 
     public static int test = 0;
 
     public static void main(String[] args) throws InterruptedException {
-
+        System.out.println("Введите время для работы и отдыха или вызовите '--help'");
         String[] cmd = new Scanner(System.in).nextLine().split(" ");
 
-        int work = 50;
-        int breake = 10;
-        int sizebreak = 30;
-        int sizework = 30;
+        //время в минутах
+        int workTime = 50;
+        int breakTime = 10;
+        int sizeBreak = 30;
+        int sizeWork = 30;
         int help = 0;
         int count = 1;
+        int multiply = 2;
+
 
         for (int i = 0; i < cmd.length; i++) {
             switch (cmd[i]) {
@@ -30,30 +32,31 @@ public class Main {
                     System.out.println(
                             "	-count <count>: количество итераций.\n");
                     System.out.println(
+                            "	-m <int>: сколько раз умножить время работы.\n");
+                    System.out.println(
                             "	--help: меню помощи.\n");
                     help = 1;
                 }
-                case "-w" -> work = Integer.parseInt(cmd[++i]);
-                case "-b" -> breake = Integer.parseInt(cmd[++i]);
+                case "-w" -> workTime = Integer.parseInt(cmd[++i]);
+                case "-b" -> breakTime = Integer.parseInt(cmd[++i]);
+                case "-m" -> multiply = Integer.parseInt(cmd[++i]);
                 case "-count" -> count = Integer.parseInt(cmd[++i]);
             }
         }
+
         if (help == 0) {
             long startTime = System.currentTimeMillis();
-            for (int i = 1; i <= count; i++) {
-                timer(work, breake, sizebreak, sizework);
+            for (int i = 1; i <= count; workTime = workTime * multiply, i++) {
+                timer(workTime, breakTime, sizeBreak, sizeWork);
             }
             long endTime = System.currentTimeMillis();
-            System.out.println("Pomodor таймер истек: " + (endTime - startTime) / (1000 * 60) + " min");
+            System.out.println("Pomodoro таймер истек: " + (endTime - startTime) / (1000 * 60) + " min");
         }
-
     }
 
-    public static void timer(int work, int breake, int sizebreak, int sizework) throws InterruptedException {
-
-        printProgress("Work Progress::  ", work, sizework);
-
-        printProgress("Break Progress:: ", breake, sizebreak);
+    public static void timer(int workTime, int breakTime, int sizeBreak, int sizeWork) throws InterruptedException {
+        printProgress("Work Progress::  ", workTime, sizeWork);
+        printProgress("Break Progress:: ", breakTime, sizeBreak);
     }
 
     private static void printProgress(String process, int time, int size) throws InterruptedException {
